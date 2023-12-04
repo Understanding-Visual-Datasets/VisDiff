19496b8f49c2312098b4e3823987a595de2f6610
Yuhui Zhang <zyh.thu@gmail.com>
2023-12-03 21:27:51 -0800

    revert vlm prompt for vlm proposer

--- components/prompts.py
+++ components/prompts.py
@@ -158,6 +158,31 @@ CLIP_FRIENDLY = """
     Again, I want to figure out what kind of distribution shift are there. List properties that hold more often for the images (not captions) in group A compared to group B. Answer with a list (separated by bullet points "*"). Your response:
 """
 
+VLM_PROMPT = """
+    This image contains two groups of images. 20 images from Group A are shown in the first two rows, while 20 images from Group B are shown in the last two rows.
+
+    I am a machine learning researcher trying to figure out the major differences between these two groups so I can better understand my data.
+
+    Come up with 10 distinct concepts that are more likely to be true for Group A compared to Group B. Please write a list of captions (separated by bullet points "*"). For example:
+    * "a dog next to a horse"
+    * "a car in the rain"
+    * "low quality"
+    * "cars from a side view"
+    * "people in a intricate dress"
+    * "a joyful atmosphere"
+
+    Do not list more than one concept. The hypothesis should be a caption, so hypotheses like "more of ...", "presence of ...", "images with ..." are incorrect. Also do not enumerate possibilities within parentheses. Here are examples of bad outputs and their corrections:
+    * INCORRECT: "various nature environments like lakes, forests, and mountains" CORRECTED: "nature"
+    * INCORRECT: "images of household object (e.g. bowl, vacuum, lamp)" CORRECTED: "household objects"
+    * INCORRECT: "Presence of baby animals" CORRECTED: "baby animals"
+    * INCORRECT: "Different types of vehicles including cars, trucks, boats, and RVs" CORRECTED: "vehicles"
+    * INCORRECT: "Images involving interaction between humans and animals" CORRECTED: "interaction between humans and animals"
+    * INCORRECT: "More realistic images" CORRECTED: "realistic images" 
+    * INCORRECT: "Insects (cockroach, dragonfly, grasshopper)" CORRECTED: "insects"
+
+    Again, I want to figure out what kind of distribution shift are there. List properties that hold more often for the images in group A compared to group B. Answer with a list (separated by bullet points "*"). Your response:
+"""
+
 CLIP_FRIENDLY_GROUP_A = """
     The following are the result of captioning a group of images:
 
